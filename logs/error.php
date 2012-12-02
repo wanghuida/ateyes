2a1aa0755e38f6402efcf026f4ad1cb6fa91a849
william <williamwang@anjuke.com>
2012-12-02 16:04:58 +0800

    set width

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2012-12-02 07:57:41 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2012-12-02T07:57:41+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
